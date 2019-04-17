4d900fb9afa00079ea302c9689bf831cec63a249
Daniel Georgiev <me@dgeorgiev.biz>
2019-04-17 20:50:00 +0300

    Add more apps and also group them by type

--- install-osx-dev.sh
+++ install-osx-dev.sh
@@ -1,7 +1,30 @@
-brew install pass
+# dotfiles manager
 brew tap bashdot/tap
 brew install bashdot
 
+# password manager
 brew cask install gpg-suite
-brew cask install spotify
+brew install pass
+
+# dev
+brew cask install vagrant
 brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install virtualbox
+brew cask install cyberduck
+brew cask install caskroom/versions/firefox-developer-edition
+
+# design
+brew cask install adobe-creative-cloud
+# to install run:
+# open '/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app'
+brew cask install sketch
+
+# utilities
+brew cask install rescuetime
+brew cask install spotify
+
+# work
+brew cask install slack
+brew cask install google-chrome
+brew cask install 1password

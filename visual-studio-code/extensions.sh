a449d51f2e42857b0e750da38547cf0cb77f066a
Daniel Georgiev <me@dgeorgiev.biz>
2019-04-18 17:03:56 +0300

    Update settings and add some extensions

---
+++ visual-studio-code/extensions.sh
@@ -0,0 +1,4 @@
+Code --install-extension ms-vscode.sublime-keybindings
+Code --install-extension dbaeumer.vscode-eslint
+Code --install-extension bmewburn.vscode-intelephense-client
+Code --install-extension felixfbecker.php-debug

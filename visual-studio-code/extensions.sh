e2108af5c7a2f0823d9d20986102f7674be77c95
Daniel Georgiev <me@dgeorgiev.biz>
2019-05-13 14:34:05 +0300

    Update some color and editor settings

--- visual-studio-code/extensions.sh
+++ visual-studio-code/extensions.sh
@@ -2,3 +2,5 @@ Code --install-extension ms-vscode.sublime-keybindings
 Code --install-extension dbaeumer.vscode-eslint
 Code --install-extension bmewburn.vscode-intelephense-client
 Code --install-extension felixfbecker.php-debug
+Code --install-extension teabyii.ayu
+# Code --install-extension karyfoundation.theme-karyfoundation-themes

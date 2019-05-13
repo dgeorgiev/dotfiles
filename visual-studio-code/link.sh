c1dc7afa6db49731bac4389d0bfa6e089b068ecf
Daniel Georgiev <me@dgeorgiev.biz>
2019-05-13 14:33:51 +0300

    Move to vscodium

--- visual-studio-code/link.sh
+++ visual-studio-code/link.sh
@@ -1,1 +1,3 @@
-ln -sf $(pwd)/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+# original vscode path
+# ln -sf $(pwd)/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+ln -sf $(pwd)/settings.json "$HOME/Library/Application Support/VSCodium/User/settings.json"

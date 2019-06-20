6f88b116b4d8b4ef63adf3fc2cf636a1d0efc1e4
dgeorgiev <daniel@dabanny.com>
2019-06-20 21:19:09 +0300

    Remove vagrant, virtualbox, php7.1 and 1password

--- install-osx-dev.sh
+++ install-osx-dev.sh
@@ -7,15 +7,12 @@ brew cask install gpg-suite
 brew install pass
 
 # dev
-brew cask install vagrant
 brew cask install iterm2
 brew cask install visual-studio-code
-brew cask install virtualbox
 brew cask install cyberduck
 brew cask install caskroom/versions/firefox-developer-edition
 brew install node
 brew install yarn
-brew install php@7.1
 brew install composer
 
 brew install coreutils
@@ -38,6 +35,3 @@ brew cask install spotify
 # work
 brew cask install slack
 brew cask install google-chrome
-brew cask install 1password
-
-vagrant plugin install vagrant-vbguest

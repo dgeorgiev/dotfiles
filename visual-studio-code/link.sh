200a4fe2973f00b6fb5cffdbd3e1975dec2138fc
dgeorgiev <daniel@dabanny.com>
2019-06-21 21:56:46 +0300

    Switch to microsoft build again

--- visual-studio-code/link.sh
+++ visual-studio-code/link.sh
@@ -1,3 +1,3 @@
 # original vscode path
-# ln -sf $(pwd)/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
-ln -sf $(pwd)/settings.json "$HOME/Library/Application Support/VSCodium/User/settings.json"
+ln -sf $(pwd)/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+#ln -sf $(pwd)/settings.json "$HOME/Library/Application Support/VSCodium/User/settings.json"
